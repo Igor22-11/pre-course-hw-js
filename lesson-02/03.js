911a16847e2973384e967a899f1d6c246f6b9fa1
Igor Bespalov <igorbespalov89@gmail.com>
2024-06-07 22:44:53 +0300

    finish task 3 lesson 2 upd

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -6,3 +6,11 @@ const giveMeRightNumbers = (value1, value2, maximum) => {
   }
 }
 giveMeRightNumbers(280, 210, 209);
+
+const calls = spy.getCalls();
+if (calls.length >= 12) {
+    const task3ConsoleCall = calls[11].lastArg;
+    console.log(task3ConsoleCall);
+} else {
+    console.log("Not enough console.log() calls recorded.");
+}
